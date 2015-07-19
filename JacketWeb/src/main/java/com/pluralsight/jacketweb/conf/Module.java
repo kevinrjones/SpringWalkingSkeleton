93091c8f921e7af46baf18bbaeae27db04d50c46
Kevin Jones <kevin@rocksolidknowledge.com>
2015-07-19 18:04:44 +0100

    Work done to get the web-app up and running

---
+++ JacketWeb/src/main/java/com/pluralsight/jacketweb/conf/Module.java
@@ -0,0 +1,16 @@
+package com.pluralsight.jacketweb.conf;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
+
+@Singleton
+public class Module extends AbstractModule {
+
+
+    protected void configure() {
+
+        // bind your injections here!
+
+    }
+
+}
